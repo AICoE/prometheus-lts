669485b6a917225ad7f7f3be549b8cd4a7c213d4
Marcel Hild <hild@b4mad.net>
2019-03-01 20:50:38 +0100

    create grafana ds

---
+++ grafana_create_datasource_thanos.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -x
+datasource_name=thanos
+prometheus_namespace=aiops-prod-prometheus-lts
+sa_reader=prometheus
+protocol=https://
+
+payload="$( mktemp )"
+cat <<EOF >"${payload}"
+{
+"name": "${datasource_name}",
+"type": "prometheus",
+"typeLogoUrl": "",
+"access": "proxy",
+"url": "https://$( oc get route thanos-query -n "${prometheus_namespace}" -o jsonpath='{.spec.host}' )",
+"basicAuth": false,
+"withCredentials": false,
+"jsonData": {
+    "tlsSkipVerify":true,
+    "httpHeaderName1":"Authorization"
+},
+"secureJsonData": {
+    "httpHeaderValue1":"Bearer $( oc sa get-token "${sa_reader}" -n "${prometheus_namespace}" )"
+}
+}
+EOF
+
+cat ${payload}
+
+# setup grafana data source
+grafana_host="${protocol}$( oc get route grafana -o jsonpath='{.spec.host}' )"
+curl --insecure -H "Content-Type: application/json" -u admin:admin "${grafana_host}/api/datasources" -X POST -d "@${payload}"
